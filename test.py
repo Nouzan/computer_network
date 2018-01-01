8b7b514411662e5fa2b562691a05422f872b9a9d
Nouzan <clobraindie@outlook.com>
2018-01-01 18:54:09 +0800

    pipe

--- test.py
+++ test.py
@@ -1,17 +1,35 @@
-import os
-import time
+import os, time, fcntl
 
 PIDS = []
+R, W = os.pipe()
+# fcntl.fcntl(R, fcntl.F_SETFL, os.O_NONBLOCK)
+
 
 def child():
-    print("I am a child %s, my parent is %s" % (os.getpid(), os.getppid()))
+    count = 0
+    while True:
+        # os.close(W)
+        # time.sleep(1)
+        line = os.read(R, 64)
+        # print(int(line))
+        if int(line) == os.getpid() or count >= 50:
+            print('STOP %s' % os.getpid())
+            break
+        count += 1
+        print("[%s, %s, count=%s]" % (os.getpid(), os.getppid(), count))
 
 
 def parent():
+    w = os.fdopen(W, 'w')
     print(PIDS, os.getpid())
+    for pid in PIDS:
+        print("killing %s" % pid)
+        w.write(str(pid))
+        w.flush()
+
 
 pid = None
-count = 100
+count = 2
 
 while pid != 0 and count > 0:
     pid = os.fork()
@@ -22,3 +40,4 @@ if pid == 0:
     child()
 else:
     parent()
+    os.wait()
